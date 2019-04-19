31d1050126b29d560ee928d2595db88c5bd9cd92
Marc Hermon <hermonm@urbandaleschools.com>
2019-04-19 14:57:33 -0500

    BB8 Attack

---
+++ BB8_Attack_Starter_Code.py
@@ -0,0 +1,35 @@
+import random
+import arcade
+
+# --- Constants ---
+BB8_scale = 0.3
+trooper_scale = 0.1
+trooper_count = 40
+SW = 800
+SH = 600
+
+#------MyGame Class--------------
+class MyGame(arcade.Window):
+
+    def __init__(self,SW,SH,title):
+        super().__init__(SW, SH, title)
+        arcade.set_background_color(arcade.color.SKY_BLUE)
+
+    def on_draw(self):
+        arcade.start_render()
+
+    def update(self, dt):
+        pass
+
+
+
+#-----Main Class--------
+def main():
+    window = MyGame(SW,SH,"BB8 Attack")
+    arcade.run()
+
+
+
+#------Run Main----------
+if __name__ == "__main__":
+    main()
