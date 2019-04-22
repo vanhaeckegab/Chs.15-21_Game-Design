7f64335f274566eac2b1994042eada972af753db
Marc Hermon <hermonm@urbandaleschools.com>
2019-04-22 12:48:55 -0500

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,14 @@
+'''
+SPRITE GAME
+-----------
+Here you will start the beginning of a game that you will be able to update as we
+learn more in the following chapters. Below are the criteria/constraints that you
+need to include:
+
+1.) Find some new sprite images.
+2.) Move the player sprite with arrow keys rather than the mouse. Don't let it move off the screen.
+3.) Move the other sprites in some way like moving down the screen and then re-spawning above the window.
+4.) Use sounds when a sprite is killed or the player hits the sidewall.
+5.) See if you can reset the game after 30 seconds. Remember the on_update() method runs every 1/60th of a second.
+6.) Try some other creative ideas to make your game awesome. 
+'''
