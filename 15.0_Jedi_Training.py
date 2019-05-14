c462eee822c1545bcf5806a13049e2ff95d866b0
Marc Hermon <hermonm@urbandaleschools.com>
2019-05-14 12:49:37 -0500

    Update and rename 15.0_Take_Home_Test.py to 15.0_Jedi_Training.py

--- 15.0_Jedi_Training.py
+++ 15.0_Jedi_Training.py
@@ -1,10 +1,7 @@
-'''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+#Sign your name:________________
  
-You will use the starting code below and build the program "BB8 Attack" as you go through Chapter 15.
+#You will use the starting code below and build the program "BB8 Attack" as you go through Chapter 15.
 
-'''
 
 import random
 import arcade
