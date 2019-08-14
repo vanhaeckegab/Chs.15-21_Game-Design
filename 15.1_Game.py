11bc1a857f0ce7390a695b4540df68238cdc1b10
Marc Hermon <hermonm@urbandaleschools.com>
2019-08-14 10:42:35 -0500

    Update 15.1_Game.py

--- 15.1_Game.py
+++ 15.1_Game.py
@@ -2,8 +2,7 @@
 SPRITE GAME
 -----------
 Here you will start the beginning of a game that you will be able to update as we
-learn more in upcoming chapters. Below are the criteria/constraints that you
-need to include:
+learn more in upcoming chapters. Below are some ideas that you could include:
 
 1.) Find some new sprite images.
 2.) Move the player sprite with arrow keys rather than the mouse. Don't let it move off the screen.
@@ -11,5 +10,37 @@ need to include:
 4.) Use sounds when a sprite is killed or the player hits the sidewall.
 5.) See if you can reset the game after 30 seconds. Remember the on_update() method runs every 1/60th of a second.
 6.) Try some other creative ideas to make your game awesome. Perhaps collecting good sprites while avoiding bad sprites.
+7.) Keep score and use multiple levels. How do you keep track of an all time high score?
+8.) Make a two player game.
 
 '''
+
+import random
+import arcade
+
+# --- Constants ---
+SW = 800
+SH = 600
+
+#------MyGame Class--------------
+class MyGame(arcade.Window):
+
+    def __init__(self,SW,SH,title):
+        super().__init__(SW, SH, title)
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        arcade.start_render()
+
+    def on_update(self, dt):
+        pass
+
+
+#-----Main Function--------
+def main():
+    window = MyGame(SW,SH,"My Game")
+    arcade.run()
+
+#------Run Main Function-----
+if __name__ == "__main__":
+    main()
